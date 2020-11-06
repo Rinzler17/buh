4d9728859ef63268596212bbbb19974d5fcbc4b9
antonturilov <67686360+antonturilov@users.noreply.github.com>
2020-11-06 18:05:29 +0500

    add files
    first upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>buh</title>
+</head>
+<body>
+<?php include header.php ?>
+<?php include main.php ?>
+<?php include footer.php ?>
+</body>
+</html>
