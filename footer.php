7dc996c726f99d2abcc7948647b5840b722c057b
antonturilov <67686360+antonturilov@users.noreply.github.com>
2020-11-20 12:25:01 +0500

    footer php

--- footer.php
+++ footer.php
@@ -1,3 +1,15 @@
 <footer>
-    footer
+    <nav>
+        <div class="container">
+            <div>©2020 "ИП Турилова О.В." <a href="privacy.html">Политика конфиденциальности</a></div>
+            <div class="social">
+                <a href="https://vk.com/id106121354"><img src="img/icons/vk.svg" alt=""></a>
+                <a href="https://www.instagram.com/kseniya_fins/"><img src="img/icons/instagram.svg" alt=""></a>
+            </div>
+            <div class="contacts">
+                <a href="tel:+79173572792">+7 917 357 27 92</a>
+                <a href="mailto:antonturilov@gmail.com">antonturilov@gmail.com</a>
+            </div>
+        </div>
+    </nav>
 </footer>
---
+++ price.php
@@ -0,0 +1,126 @@
+<head>
+    <link type="text/css" rel="stylesheet" href="css/main.css">
+    <meta charset="UTF-8">
+    <title>price</title>
+</head>
+<body>
+<header>
+    <nav>
+        <div class="container">
+            <p class="middle"><a href="index.html">Главная</a></p>
+            <p class="middle"><a href="#tariffs">Услуги</a></p>
+            <p class="middle"><a href="#review">Отзывы</a></p>
+            <p class="middle"><a href="price.html">Цены</a></p>
+            <p class="middle"><a href="#team">Контакты</a></p>
+            <p class="middle"><a href="tel:+79173572792">+7 917 357 27 92</a></p>
+            <span>Обратный звонок</span>
+        </div>
+    </nav>
+</header>
+<main>
+    <section>
+        <div class="container">
+            <div class="price" style="padding: 0;">
+                <div class="items">
+
+                    <div class="item-group">
+                        <div class="gropu-title">Бухгалтерские услуги</div>
+
+                        <div class="item">
+                            <div class="item-title">Постановка бухгалтерского учета</div>
+                            <div class="item-price">от 5 000</div>
+                        </div>
+                        <div class="item">
+                            <div class="item-title">Сдача нулевой отчетности ИП</div>
+                            <div class="item-price">от 1000</div>
+                        </div>
+                        <div class="item">
+                            <div class="item-title">Сдача нулевой отчетности ООО</div>
+                            <div class="item-price">от 3000</div>
+                        </div>
+                        <div class="item">
+                            <div class="item-title">Подготовка отчетности ИП</div>
+                            <div class="item-price">от 4000</div>
+                        </div>
+                        <div class="item">
+                            <div class="item-title">Подготовка отчетности ООО</div>
+                            <div class="item-price">от 5000</div>
+                        </div>
+                        <div class="item">
+                            <div class="item-title">Бухгалтерское сопровождение ИП</div>
+                            <div class="item-price">от 5000</div>
+                        </div>
+                        <div class="item">
+                            <div class="item-title">Бухгалтерское сопровождение ООО</div>
+                            <div class="item-price">от 6000</div>
+                        </div>
+                    </div>
+
+
+                    <div class="item-group">
+                        <div class="gropu-title">Кадровые услуги</div>
+                        <div class="item">
+                            <div class="item-title">Постановка кадрового делопроизводства</div>
+                            <div class="item-price">от 3000</div>
+                        </div>
+                        <div class="item">
+                            <div class="item-title">Подготовка отчетности по сотрудникам</div>
+                            <div class="item-price">от 10000</div>
+                        </div>
+                    </div>
+
+                    <div class="item-category">
+                        <div class="gropu-title">Внешнеэкономическая деятельность</div>
+                        <div class="item">
+                            <div class="item-title">Валютный контроль в банке</div>
+                            <div class="item-price">от 3000 Р</div>
+                        </div>
+                        <div class="item">
+                            <div class="item-title">Подготовка отчетности в ФТС</div>
+                            <div class="item-price">от 3000 Р</div>
+                        </div>
+                    </div>
+
+
+                    <div class="item-category">
+                        <div class="gropu-title">Юридические услуги</div>
+                        <div class="item">
+                            <div class="item-title">Открытие/закрытие ИП</div>
+                            <div class="item-price">от 2000 Р</div>
+                        </div>
+                        <div class="item">
+                            <div class="item-title">Регистрация ООО</div>
+                            <div class="item-price">от 5000 Р</div>
+                        </div>
+                        <div class="item">
+                            <div class="item-title">Внесение изменений в ЕГРЮЛ</div>
+                            <div class="item-price">от 5000 Р</div>
+                        </div>
+                    </div>
+
+
+                    <div class="item-category">
+                        <div class="gropu-title">Консультирование</div>
+                        <div class="item">
+                            <div class="item-title">Налоговое и юридическое консультирование</div>
+                            <div class="item-price">от 3000 Р</div>
+                        </div>
+                        <div class="item">
+                            <div class="item-title">Финансовое консультирование</div>
+                            <div class="item-price">от 5000 Р</div>
+                        </div>
+
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php include footer.php; ?>
+
+
+</body>
+</html>
